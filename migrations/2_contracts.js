dc720104e286eb2d6672e5c29e2db367cddedfa3
taygius <taygius@gmail.com>
2018-01-24 18:09:45 +0300

    Fixed migrations: all tests passed

--- migrations/2_contracts.js
+++ migrations/2_contracts.js
@@ -4,18 +4,24 @@ var Platform = artifacts.require("Platform");
 
 var fs = require('fs');
 
-module.exports = async function(deployer, network)  {
-  await deployer.deploy(Scrinium);
-  await deployer.deploy(Balances, Scrinium.address);
-  await deployer.deploy(Platform);
+module.exports = function(deployer, network){
 
-  if (network !== 'testing') { // do not use it for tests
-    var code =`# preload.js for geth client
+  deployer.deploy(Scrinium)
+    .then(function () {
+      return deployer.deploy(Balances, Scrinium.address);
+    })
+    .then(function () {
+      return deployer.deploy(Platform);
+    })
+    .then(function () {
+
+      if (network !== 'testing') { // do not use it for tests
+        var code =`// preload.js for geth client
 var scrinium = eth.contract(JSON.parse('${JSON.stringify(Scrinium.abi)}')).at('${Scrinium.address}');
 var balances = eth.contract(JSON.parse('${JSON.stringify(Balances.abi)}')).at('${Balances.address}');
 var platform = eth.contract(JSON.parse('${JSON.stringify(Platform.abi)}')).at('${Platform.address}');
 `;
-    await fs.writeFileSync('./preload.js', code);
-  }
-
+        return fs.writeFileSync('./preload.js', code);
+      }
+    });
 };
